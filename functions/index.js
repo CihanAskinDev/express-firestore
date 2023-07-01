3d2a94caf8d83e5a38ed320b72703b30d6cfd24e
Cihan <cihan@Cihans-MacBook-Pro.local>
2023-07-01 14:39:29 -0400

    first commit

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { onRequest } from "firebase-functions/v2/https";
 import cors from "cors"
 import {  getCandy, addNewCandy  } from "./src/candy.js"
 
@@ -10,6 +11,8 @@ app.use(express.json())
 
 app.get("/candy", getCandy)
 app.post("/candy", addNewCandy)
-app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}...`)
-})
+//app.listen(PORT, () => {
+  //  console.log(`Listening on http://localhost:${PORT}...`)
+// })
+
+export const api = onRequest(app);
